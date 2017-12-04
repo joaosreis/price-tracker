3972cea28680c37f8d97ed2480dec34cb83502bf
João Santos Reis <joaosreis@outlook.pt>
2017-12-04 18:09:07 +0000

    Updated Fnac parser, new price method

--- src/parsers/fnac.ml
+++ src/parsers/fnac.ml
@@ -1,7 +1,11 @@
 let get_price html =
   let open Soup in
   let soup = parse html in
-  try Price.Stock (soup $ "div.ProductPriceBox-item" |> R.attribute "data-price" |> float_of_string) with _ -> Price.NoStock
+  let div = soup $ "div#hl_1" in
+  if div |> R.attribute "data-in-stock" |> int_of_string = 0 then
+    Price.NoStock
+  else
+    Price.Stock (div |> R.attribute "data-price" |> float_of_string)
 
 let get_name html =
   let open Soup in
